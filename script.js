c85e3a0a517474931fad8b8750553dc6ea29011e
mustafa <mltm0204@gmail.com>
2021-11-18 21:52:09 +0300

    JS codes are added

--- script.js
+++ script.js
@@ -12,9 +12,26 @@ movieSelectBox.addEventListener("change", (e)=> {
 });
 
 const updateMovieInfo = (filmPrice)=>{
-    let selectedSeats = document.querySelectorAll(".row .seat .selected");
+    let selectedSeats = document.querySelectorAll(".row .selected");
 
+    // const seatIndexArray = [...selectedSeats].map(seat => [...notOccupiedSeats].indexOf(seat));
 
-    const seatIndexArray = [...selectedSeats].map(seat => [...notOccupiedSeats].indexOf(seat));
+    const selectedSeatCount = selectedSeats.length;
+    count.innerText = selectedSeatCount;
+    film.innerText = movieSelectBox.options[movieSelectBox.selectedIndex].innerText.split(" ( ")[0];
+    // console.log(film.innerText);
+    total.innerText = selectedSeatCount * parseFloat(filmPrice);
 
 }
+
+container.addEventListener("click", e =>{
+    console.log(e.target);
+
+    if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
+        e.target.classList.toggle("selected"); 
+        // e.target.classList.add("selected")        
+    }
+
+    let price = movieSelectBox.options[movieSelectBox.selectedIndex].value;
+    updateMovieInfo(price);
+});
